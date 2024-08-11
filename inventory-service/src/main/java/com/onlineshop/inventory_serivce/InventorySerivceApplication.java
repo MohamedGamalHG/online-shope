8c1d9d77876e8592d7e65f1976fa8a2d08f48b88
MohamedGamalHG <mohamedgemmyhamzagaber@gmail.com>
2024-08-11 15:19:47 +0300

    add:add data when app run in inventory service

--- inventory-service/src/main/java/com/onlineshop/inventory_serivce/InventorySerivceApplication.java
+++ inventory-service/src/main/java/com/onlineshop/inventory_serivce/InventorySerivceApplication.java
@@ -1,7 +1,11 @@
 package com.onlineshop.inventory_serivce;
 
+import com.onlineshop.inventory_serivce.entity.jpa.JpaInventory;
+import com.onlineshop.inventory_serivce.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventorySerivceApplication {
@@ -10,4 +14,20 @@ public class InventorySerivceApplication {
 		SpringApplication.run(InventorySerivceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository)
+	{
+		return args -> {
+			JpaInventory inventory = new JpaInventory();
+			inventory.setSkuCode("Iphone_13");
+			inventory.setQuantity(12);
+
+			JpaInventory inventory1 = new JpaInventory();
+			inventory1.setSkuCode("Iphone_14");
+			inventory1.setQuantity(15);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
 }
